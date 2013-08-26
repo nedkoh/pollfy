a2549fa0604ab63c41292714d11512015f773a0e
nedkoh <nedkoh@gmail.com>
2013-08-26 19:27:39 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pollfy::Application.config.secret_key_base = '7167294e43973b441d11aed896bbcb0526b13db229abedfae2e1b479afab581d567cd2d2df3c7a1e28c41d1c9f332a6409dbb0789bacfb2ed8afe98ad37bd8e1'
