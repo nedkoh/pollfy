563e7cafc8454f20cb645c9880dd073ce476a8e9
nedkoh <nedkoh@gmail.com>
2013-09-20 16:55:09 -0400

    added seeds for new DBs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#create some roles
+["admin", "member", "paidm"].each do |role|
+  Role.find_or_create_by_name(role)
+end
+
+#create some users
+role = Role.find(1)
+User.create!(:id => 1, :email => "admin@gmail.com", :password => "nji9NJI(", :encrypted_password => "$2a$10$PtXUTYlBN.DRINuWHlda8.w7CXpPTEuPaNELPVAkf62nCu3HOr5V.", :roles => [role])
+User.create!(:id => 2, :email => "member@gmail.com", :password => "nji9NJI(", :encrypted_password => "$2a$10$PtXUTYlBN.DRINuWHlda8.w7CXpPTEuPaNELPVAkf62nCu3HOr5V.")
+paidm = Role.find(3)
+User.create!(:id => 2, :email => "koko@gmail.com", :password => "nji9NJI(", :encrypted_password => "$2a$10$PtXUTYlBN.DRINuWHlda8.w7CXpPTEuPaNELPVAkf62nCu3HOr5V.", :roles => [paidm])
+
+puts "Ready to rock!"
