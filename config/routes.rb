97c51e241f16950fb26427fa08c9582ab688f8c8
nedkoh <nedkoh@gmail.com>
2013-10-01 19:31:33 -0400

    restructuring comments under surveys

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Pollfy::Application.routes.draw do
     resources :questions
   end
 
-  get 'profile', to: 'users#edit'
+  #get 'profile', to: 'users#edit'
+  #get 'profile', to: 'devise/registrations#edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
